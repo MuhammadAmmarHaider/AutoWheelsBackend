157e08483def44d9abcd7dc6f543190c3e3c70b7
Saad7912 <muhammadsaadtariq2003@gmail.com>
2026-05-03 23:12:03 +0500

    make some changes in new car feature

--- src/new-car/new-car.module.ts
+++ src/new-car/new-car.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NewCarService } from './new-car.service';
 import { NewCarController } from './new-car.controller';
-
+// Import the PrismaModule to allow NewCarService to interact with the database
 @Module({
   providers: [NewCarService],
   controllers: [NewCarController]
