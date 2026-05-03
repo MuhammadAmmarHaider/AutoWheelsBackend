c5471e4faa4500a3bb05ee677e4aaa6fcffcad0e
Saad7912 <muhammadsaadtariq2003@gmail.com>
2026-05-03 23:14:27 +0500

    complete the favorite features

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { FavoriteController } from './favorite.controller';
 import { FavoriteService } from './favorite.service';
-
+// The FavoriteModule is responsible for managing user favorites, allowing users to add, remove, and retrieve their favorite listings. It includes the FavoriteController for handling HTTP requests and the FavoriteService for business logic related to favorites.
 @Module({
     controllers: [FavoriteController],
     providers: [FavoriteService],
